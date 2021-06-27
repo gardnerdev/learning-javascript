4d511c1100b2de80c2473afb2cf5da40ecd2c82b
gardnerdev <24687623+gardnerdev@users.noreply.github.com>
2021-06-27 19:57:28 +0000

    Added counting words (using reduce function)

---
+++ izogram/index.js
@@ -0,0 +1,41 @@
+/*
+* Opanuj JavaScript - Przeprogramowani.pl
+* I. Fundamenty języka JavaScript
+*
+* Ćwiczenie 13 - "Izogram"
+*/
+
+/*
+* Cel zadania
+*------------
+* Zaimplementuj funkcję sprawdzającą czy podane słowo jest izogramem, czyli słowem w którym każda litera występuje tylko jeden raz.
+*
+*
+* Przykład:
+* isogram('Izogram'); // => true
+* isogram('Przeprogramowani'); // => false
+* 
+*/
+
+function isogram(word) {
+  for (let i = 0; i < word.length; i++) {
+    if (word.indexOf(word[i]) !== word.lastIndexOf(word[i])) {
+      return true
+    }
+  }
+  return false
+}
+
+/* Weryfikacja */
+
+function verify(input, goal) {
+  if (input === goal) {
+    console.log('Gratulacje!');
+  } else {
+    console.log(`Niestety, oczekiwano - ${goal}, otrzymano - ${input}`);
+  }
+}
+
+verify(isogram('izogram'), true);
+verify(isogram('Przeprogramowani'), false);
+verify(isogram('SprawdzAm'), false);
